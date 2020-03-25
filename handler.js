64b56c34970e0c7c260d7150bd6453f83ba36250
Mario <mario.martinez@secretescapes.com>
2020-03-25 16:25:04 +0100

    changes

--- handler.js
+++ handler.js
@@ -1,18 +1,134 @@
-'use strict';
+"use strict";
+const axios = require("axios");
+const AWS = require("aws-sdk");
 
-module.exports.hello = async event => {
+require("dotenv").config();
+
+// TODO: Change endpoint!!
+const dynamodb = new AWS.DynamoDB({
+  region: "eu-west-1",
+  endpoint: "http://localhost:8000"
+});
+
+module.exports.collectTestResults = async event => {
+  const buildId = event.pathParameters.buildId;
+  if (!buildId) {
+    return {
+      statusCode: 400,
+      body: "You have to provide a build id"
+    };
+  }
+
+  const tests = await fetchBuild(buildId);
+  await storeResults(tests);
+
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
+    body: JSON.stringify(tests)
+  };
+};
+
+async function storeResults(testResults) {
+  await testResults.forEach(async testResult => {
+    try {
+      if (testResult.status !== "PASSED") {
+        const key = `${testResult.className}.${testResult.name}`;
+        const response = await getResultsFromDB(testResult, key);
+        if (response.Item) {
+          const existingTestResults = Array.from(
+            JSON.parse(response.Item.results.S)
+          );
+
+          existingTestResults.push(testResult);
+
+          const updateItemParams = {
+            Key: {
+              test_name: {
+                S: key
+              }
+            },
+            UpdateExpression: `SET results = :value`,
+            ExpressionAttributeValues: {
+              ":value": { S: JSON.stringify(existingTestResults) }
+            },
+            TableName: "test_results"
+          };
+          await dynamodb.updateItem(updateItemParams).promise();
+        } else {
+          const putItemParams = {
+            Item: {
+              test_name: {
+                S: key
+              },
+              results: {
+                S: `[${JSON.stringify(testResult)}]`
+              }
+            },
+            TableName: "test_results"
+          };
+          await dynamodb.putItem(putItemParams).promise();
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+}
+
+async function getResultsFromDB(testResult, key) {
+  const params = {
+    TableName: "test_results",
+    Key: {
+      test_name: {
+        S: key
+      }
+    }
+  };
+  return await dynamodb.getItem(params).promise();
+}
+
+async function fetchBuild(buildId) {
+  const buildInfoResponse = await axios.get(
+    `https://jenkins.secretescapes.com/job/secret-escapes/job/master/${buildId}/api/json`,
+    {
+      auth: {
+        username: process.env.JENKINS_USERNAME,
+        password: process.env.JENKINS_PASSWORD
+      }
+    }
+  );
+
+  const buildInfo = {
+    buildTimestamp: buildInfoResponse.data.timestamp,
+    buildResult: buildInfoResponse.data.result,
+    buildId: buildInfoResponse.data.id
   };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+  const testReportResponse = await axios.get(
+    `https://jenkins.secretescapes.com/job/secret-escapes/job/master/${buildId}/testReport/api/json`,
+    {
+      auth: {
+        username: process.env.JENKINS_USERNAME,
+        password: process.env.JENKINS_PASSWORD
+      }
+    }
+  );
+
+  const testResults = testReportResponse.data.suites.flatMap(suite =>
+    suite.cases.flatMap(c => ({
+      ...buildInfo,
+      name: c.name,
+      className: c.className,
+      status: c.status == "PASSED" ? c.status : "FAILED",
+      duration: c.duration,
+      errorDetails: c.errorDetails,
+      errorStackTrace: c.errorStackTrace,
+      skipped: c.skipped,
+      skippedMessage: c.skippedMessage
+    }))
+  );
+
+  console.log(`Response received with code: ${testReportResponse.status}`);
+
+  return testResults;
+}
