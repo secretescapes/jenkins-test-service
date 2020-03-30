3db1261fd52b23d12809726a9acaf7b127a80458
Mario <mario.martinez@secretescapes.com>
2020-03-30 13:36:27 +0200

    changes

--- handler.js
+++ handler.js
@@ -13,44 +13,60 @@ if (process.env.LOCAL_DYNAMODB_ENDPOINT !== "") {
 const dynamodb = new AWS.DynamoDB(dynamoDbParams);
 
 module.exports.collectTestResults = async event => {
-  const buildId = event.pathParameters.buildId;
-  if (!buildId) {
+  try {
+    const buildId = event.pathParameters.buildId;
+    if (!buildId) {
+      return {
+        statusCode: 400,
+        body: "You have to provide a build id"
+      };
+    }
+
+    const tests = await fetchBuild(buildId);
+    await storeResults(tests);
+
+    console.log(`Successfuly collected results for ${buildId}`);
     return {
-      statusCode: 400,
-      body: "You have to provide a build id"
+      statusCode: 200
+    };
+  } catch (err) {
+    console.error(`ERROR: ${err}`);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(err)
     };
   }
-
-  const tests = await fetchBuild(buildId);
-  await storeResults(tests);
-
-  console.log(`Successfuly collected results for ${buildId}`);
-  return {
-    statusCode: 200,
-    body: JSON.stringify(tests)
-  };
 };
 
 async function storeResults(testResults) {
-  await testResults.forEach(async testResult => {
+  console.log(
+    `[START] StoreResults for ${testResults ? testResults.length : 0} results`
+  );
+  for (const testResult of testResults) {
     try {
-      if (testResult.status !== "PASSED") {
-        const key = `${testResult.className}.${testResult.name}`;
-        const response = await getResultsFromDB(key);
-        if (response.Item) {
-          await updateResultsInDB(
-            key,
-            Array.from(JSON.parse(response.Item.results.S)),
-            testResult
-          );
-        } else {
-          await putResultsInDB(key, testResult);
-        }
+      const key = `${testResult.className}.${testResult.name}`;
+      console.log(`Getting results for ${key}`);
+      const response = await getResultsFromDB(key);
+      console.log(
+        `Dynamodb getItem response: ${JSON.stringify(response.Item)}`
+      );
+      if (response.Item && response.Item.results) {
+        await updateResultsInDB(
+          key,
+          Array.from(response.Item.results.SS),
+          testResult
+        );
+        console.log(`Results updated`);
+      } else {
+        console.log(`Results for ${key} not found, storing results`);
+        await putResultsInDB(key, testResult);
+        console.log(`Results stored`);
       }
     } catch (err) {
       console.error(err);
     }
-  });
+  }
+  console.log(`[END] StoreResults`);
 }
 
 async function putResultsInDB(key, testResult) {
@@ -59,13 +75,21 @@ async function putResultsInDB(key, testResult) {
       test_name: {
         S: key
       },
+      failed_in: {
+        SS: [testResult.buildId]
+      },
       results: {
-        S: `[${JSON.stringify(testResult)}]`
+        SS: [`${JSON.stringify(testResult)}`]
       }
     },
-    TableName: "test_results"
+    TableName: process.env.TABLE_NAME
   };
-  await dynamodb.putItem(putItemParams).promise();
+  try {
+    await dynamodb.putItem(putItemParams).promise();
+  } catch (err) {
+    console.error(`Error putItem: ${err}`);
+    throw err;
+  }
 }
 
 async function updateResultsInDB(key, existingTestResults, newTestResults) {
@@ -74,36 +98,51 @@ async function updateResultsInDB(key, existingTestResults, newTestResults) {
       .map(result => result.buildId)
       .includes(newTestResults.buildId)
   ) {
-    existingTestResults.push(newTestResults);
-
     const updateItemParams = {
       Key: {
         test_name: {
           S: key
         }
       },
-      UpdateExpression: `SET results = :value`,
+      UpdateExpression: `ADD #results :value, #failed_in :buildId`,
       ExpressionAttributeValues: {
-        ":value": { S: JSON.stringify(existingTestResults) }
+        ":value": { SS: [JSON.stringify(newTestResults)] },
+        ":buildId": { SS: [newTestResults.buildId] }
       },
-      TableName: "test_results"
+      ExpressionAttributeNames: {
+        "#results": "results",
+        "#failed_in": "failed_in"
+      },
+      TableName: process.env.TABLE_NAME
     };
-    await dynamodb.updateItem(updateItemParams).promise();
+    try {
+      await dynamodb.updateItem(updateItemParams).promise();
+    } catch (err) {
+      console.error(`Error updateItem: ${err}`);
+      throw err;
+    }
   }
 }
 async function getResultsFromDB(key) {
   const params = {
-    TableName: "test_results",
+    TableName: process.env.TABLE_NAME,
     Key: {
       test_name: {
         S: key
       }
     }
   };
-  return await dynamodb.getItem(params).promise();
+  try {
+    const result = await dynamodb.getItem(params).promise();
+    return result;
+  } catch (err) {
+    console.error(`Error getting item: ${err}`);
+    throw err;
+  }
 }
 
 async function fetchBuild(buildId) {
+  console.time("fetchBuild");
   const buildInfoResponse = await axios.get(
     `https://jenkins.secretescapes.com/job/secret-escapes/job/master/${buildId}/api/json`,
     {
@@ -121,7 +160,7 @@ async function fetchBuild(buildId) {
   };
 
   const testReportResponse = await axios.get(
-    `https://jenkins.secretescapes.com/job/secret-escapes/job/master/${buildId}/testReport/api/json`,
+    `https://jenkins.secretescapes.com/job/secret-escapes/job/master/${buildId}/testReport/api/json?tree=suites[cases[className,name,age,status,duration,errorDetails,errorStackTrace,skipped,skippedMessage]]`,
     {
       auth: {
         username: process.env.JENKINS_USERNAME,
@@ -131,22 +170,24 @@ async function fetchBuild(buildId) {
   );
 
   const testResults = testReportResponse.data.suites.flatMap(suite =>
-    suite.cases.flatMap(c => ({
-      ...buildInfo,
-      name: c.name,
-      className: c.className,
-      status: c.status == "PASSED" ? c.status : "FAILED",
-      duration: c.duration,
-      errorDetails: c.errorDetails,
-      errorStackTrace: c.errorStackTrace,
-      skipped: c.skipped,
-      skippedMessage: c.skippedMessage
-    }))
+    suite.cases
+      .filter(c => c.status !== "PASSED")
+      .flatMap(c => ({
+        ...buildInfo,
+        name: c.name,
+        className: c.className,
+        status: c.status == "PASSED" ? c.status : "FAILED",
+        duration: c.duration,
+        errorDetails: c.errorDetails,
+        errorStackTrace: c.errorStackTrace,
+        skipped: c.skipped,
+        skippedMessage: c.skippedMessage
+      }))
   );
 
   console.log(
     `Response received with code: ${testReportResponse.status} for build ${buildId}`
   );
-
+  console.timeEnd("fetchBuild");
   return testResults;
 }
